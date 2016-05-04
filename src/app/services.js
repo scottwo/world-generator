f815d79660d3f7e247b0d7df60b2abd29e0358c5
scottwo <scott.w.ogden@gmail.com>
2016-05-04 07:32:58 -0600

    Initial commit.

--- src/app/services.js
+++ src/app/services.js
@@ -1,10 +1,11 @@
 import 'app/services/auth.service';
 import 'app/services/session.service';
 import 'app/services/notify.service';
-
+import 'app/services/person.service';
 
 angular.module('services', [
   'services.auth',
   'services.session',
   'services.notify',
+  'service.person'
 ]);
